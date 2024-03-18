ff5b0fa819ab70832f58fc3167ede8ac3a813594
unknown <yurinicoliche2003@gmail.com>
2024-03-18 18:57:06 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
 
 function App() {
+  const[value, setValue] = useState(0)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div>
+      <h1>Contadora usando <span>React</span></h1>
+      <h2>{value}</h2>
+
+      <button className='btn' onClick={() => setValue(value + 1)}>Aumentar</button>
+
+      <button className='btn' onClick={() => setValue(value - 1)}>Diminuir</button>
+
+      <button className='btn' onClick={() => setValue(0)}>Zerar</button>
     </div>
+    </>
   );
 }
 
